7a7fd744b2e1da9ceec37d9af55fb56415805da3
Javojav <javipelema@gmail.com>
2018-07-10 12:18:29 +0200

    Added replies

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Ja ne Bot
-This Telegram Bot will send Ja ne when you write some specific words.
-To use the bot, search for @Ja_Ne_Bot.
+
+This [Telegram](https://telegram.org/) bot written in [node.js](https://nodejs.org/), will reply "Ja ne" when you say some specific words.
+
+## Usage
+To use this bot, go to Telegram and search for **@Ja_Ne_Bot** or go to [this link]( https://t.me/Ja_Ne_Bot).
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bot = new TelegramBot(process.env.API_KEY, {
   polling: true
 });
 
-bot.onText(/\/start/, msg => bot.sendMessage(msg.chat.id, "Ja ne"));
+bot.onText(/(\/start)|(geil(e|a|er|es)?)|(v[o]+ll)|(fuk)|(fik)|(scheiße?)|(funzt)|(best?.{1,}(ev(e{0,}|a{0,})(r{0,})?))/i, msg => bot.sendMessage(msg.chat.id, "Ja ne"));
 
-bot.onText(/(ja)|(geil(e|a|er|es)?)|(voll)|(fuk)|(fik)|(scheiße?)|(funzt)|(best?.{1,}(ev(e{0,}|a{0,})(r{0,})?))/i, msg => bot.sendMessage(msg.chat.id, "Ja ne"));
+bot.onText(/(^|\s)jaja(\s|$)/i, msg => bot.sendMessage(msg.chat.id, "Salu2"));
+
+bot.onText(/ja ne/i, msg => bot.sendMessage(msg.chat.id, "Ja"));
